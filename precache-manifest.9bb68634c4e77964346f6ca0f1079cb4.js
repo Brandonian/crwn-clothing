4d1a84024235b92e79be7579726ceece036e519c
Brandon <bwwebs@gmail.com>
2020-09-01 09:50:22 -0700

    Updates

---
+++ precache-manifest.9bb68634c4e77964346f6ca0f1079cb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28868e13df4e31cf8375fcfc4c17a08",
+    "url": "/laravel-ecommerce-application/index.html"
+  },
+  {
+    "revision": "fab437b7a1980eb03540",
+    "url": "/laravel-ecommerce-application/static/css/main.2743bfd6.chunk.css"
+  },
+  {
+    "revision": "9df125324e26c5375f2e",
+    "url": "/laravel-ecommerce-application/static/js/2.cb256c35.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/laravel-ecommerce-application/static/js/2.cb256c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab437b7a1980eb03540",
+    "url": "/laravel-ecommerce-application/static/js/main.bcc1ae5e.chunk.js"
+  },
+  {
+    "revision": "c1f6bae3ee23fbf1ec85",
+    "url": "/laravel-ecommerce-application/static/js/runtime-main.dc356134.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/laravel-ecommerce-application/static/media/crown.ed28accb.svg"
+  }
+]);
